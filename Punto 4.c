4320abbe9dc0410cb4829c37bb3cb83a427db664
facu-21 <facundojohnsotovasquez@gmail.com>
2023-11-15 22:43:59 -0300

    TP N°10 Soto Facundo
    TP N°10 Soto Facundo

---
+++ Punto 4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void reord(int m[2][10]){
+	int i;
+	m[1][0]=m[0][9];
+	for(i=1;i<=9;i++){
+		m[1][i]=m[0][i-1];
+	}
+}
+void askm(int m[2][10]){
+	int i;
+	printf("Ingrese un vector de 10 elementos\n");
+	for(i=0;i<=9;i++){
+		printf("Ingrese elemento numero %d: ",i+1);
+		scanf("%d",&m[0][i]);
+	}
+}
+int main(){
+	int m[2][10]={0},i;
+	askm(m);
+	reord(m);
+	printf("\nVector nuevo: ");
+	for(i=0;i<=9;i++){
+		printf("%d ",m[1][i]);
+	}
+	return 0;
+}
